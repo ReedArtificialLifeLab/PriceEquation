d2bfca3a5a9dd78dd30dda14bbccf632bf341700
Henry Blanchette <henry@monkeyface.com>
2020-06-17 14:48:39 -0700

    visualizations: new fixed and fitted graphs; new "error" table entry; xticks are correctly labeled

--- pge-webapp-henry/scripts/gpe_graph.js
+++ pge-webapp-henry/scripts/gpe_graph.js
@@ -1,6 +1,6 @@
 const gpe_graph_layout = {
-  width: 600,
-  height: 400
+  width: 500,
+  height: 300
 }
 
 // const gpe_graph_layout = {top: 10, right: 30, bottom: 30, left: 60};
@@ -8,56 +8,70 @@ const gpe_graph_layout = {
 // gpe_graph_layout.height = 400 - gpe_graph_layout.top - gpe_graph_layout.bottom;
 
 const gpe_graph_colors = {
-  cov_Ctil_X_a: "red",
+  cov_Ctil_X_ancestors: "red",
   ave_DX: "blue",
-  Xbar_a: "green",
+  Xbar_ancestors: "green",
   DXbar: "orange",
 };
 
 function update_gpe_graph() {
-  let xs = [];
-  let ys = {
-    cov_Ctil_X_a: [],
-    ave_DX: [],
-    cov_Ctil_X_d: [],
-    Xbar_a: [],
-    DXbar: []
-  };
+  let result_keys = [
+    "cov_Ctil_X_ancestors",
+    "ave_DX",
+    "cov_Ctil_X_descendants",
+    "Xbar_ancestors",
+    "DXbar"
+  ];
+
+  let xs = {};
+  let ys = {};
+  result_keys.forEach((result_key) => {
+    xs[result_key] = [];
+    ys[result_key] = [];
+  });
+
+  function add_result(result_key, gen_index) {
+    xs[result_key].push(gen_index);
+    ys[result_key].push(gpe_result_table.get_entry(result_key, gen_index));
+  }
+
   for (var gen_index = 0;
        gen_index < gpe_result_table.rows.length;
        gen_index++)
   {
-      xs.push(gen_index);
-      ys.cov_Ctil_X_a.push(gpe_result_table.get_entry("cov_Ctil_X_ancestors", gen_index));
-      ys.ave_DX.push(gpe_result_table.get_entry("ave_DX", gen_index));
-      ys.cov_Ctil_X_d.push(gpe_result_table.get_entry("cov_Ctil_X_descendants", gen_index));
-      ys.Xbar_a.push(gpe_result_table.get_entry("Xbar_ancestors", gen_index));
-      ys.DXbar.push(gpe_result_table.get_entry("DXbar", gen_index));
+    result_keys.forEach((result_key) => {
+      add_result(result_key, gen_index);
+    });
   }
+  // include final generation in DXbar plot
+  xs.Xbar_ancestors.push(gpe_result_table.rows.length);
+  ys.Xbar_ancestors.push(gpe_result_table.get_entry(
+    "Xbar_descendants",
+    gpe_result_table.rows.length - 1));
 
-  let trace = {
-    cov_Ctil_X_a: {
+  let traces = {
+    cov_Ctil_X_ancestors: {
       name: "cov(Ctil_a, X_a)",
-      x: xs,
-      y: ys.cov_Ctil_X_a,
+      x: xs.cov_Ctil_X_ancestors,
+      y: ys.cov_Ctil_X_ancestors,
       type: "scatter"
     },
     ave_DX: {
       name: "ave(DX)",
-      x: xs,
+      x: xs.ave_DX,
       y: ys.ave_DX,
       type: "scatter"
     },
-    cov_Ctil_X_d: {
+    cov_Ctil_X_descendants: {
       name: "cov(Ctil_d, X_d)",
-      x: xs,
-      y: ys.cov_Ctil_X_d,
+      x: xs.cov_Ctil_X_descendants,
+      y: ys.cov_Ctil_X_descendants,
       type: "scatter"
     },
-    Xbar_a: {
-      name: "Xbar_a",
-      x: xs,
-      y: ys.Xbar_a,
+    Xbar_ancestors: {
+      name: "Xbar",
+      x: xs.Xbar_ancestors,
+      y: ys.Xbar_ancestors,
       type: "scatter",
       fill: "tonexty",
       fillcolor: color_from_trait(gpe_config.trait),
@@ -65,51 +79,85 @@ function update_gpe_graph() {
     },
     DXbar: {
       name: "DXbar",
-      x: xs,
+      x: xs.DXbar,
       y: ys.DXbar,
       type: "scatter"
     }
   };
 
-  function generate_plot1() {
+  function generate_plot_general_fixed() {
     let data = [
-      trace.cov_Ctil_X_a,
-      trace.ave_DX,
-      trace.cov_Ctil_X_d,
-      trace.DXbar
+      traces.cov_Ctil_X_ancestors,
+      traces.ave_DX,
+      traces.cov_Ctil_X_descendants,
+      traces.DXbar
     ];
 
     let layout = {
-      title: "PGE Results (Proportion Changes)",
+      title: "PGE Results (Fixed)",
       width: gpe_graph_layout.width,
       height: gpe_graph_layout.height,
+      xaxis: {
+        autotick: false,
+        tick0: 0,
+        dtick: 1
+      },
       yaxis: {
         range: [-1, 1]
       },
       showlegend: true
     };
 
-    Plotly.newPlot("gpe_graph_1", data, layout);
+    Plotly.newPlot("gpe_graph_general_fixed", data, layout);
   }
 
-  function generate_plot2() {
+  function generate_plot_general_fitted() {
     let data = [
-      trace.Xbar_a
+      traces.cov_Ctil_X_ancestors,
+      traces.ave_DX,
+      traces.cov_Ctil_X_descendants,
+      traces.DXbar
     ];
 
-    let layout2 = {
-      title: "PGE Results (Proportion)",
+    let layout = {
+      title: "PGE Results (Fitted)",
       width: gpe_graph_layout.width,
       height: gpe_graph_layout.height,
+      xaxis: {
+        autotick: false,
+        tick0: 0,
+        dtick: 1
+      },
+      showlegend: true
+    };
+
+    Plotly.newPlot("gpe_graph_general_fitted", data, layout);
+  }
+
+  function generate_plot_Xbar() {
+    let data = [
+      traces.Xbar_ancestors
+    ];
+
+    let layout = {
+      title: "Trait Proportion",
+      width: gpe_graph_layout.width,
+      height: gpe_graph_layout.height,
+      xaxis: {
+        autotick: false,
+        tick0: 0,
+        dtick: 1
+      },
       yaxis: {
         range: [0, 1]
       },
       showlegend: true
     };
 
-    Plotly.newPlot("gpe_graph_2", data, layout2);
+    Plotly.newPlot("gpe_graph_trait_proportion", data, layout);
   }
 
-  generate_plot1();
-  generate_plot2();
+  generate_plot_general_fixed();
+  generate_plot_general_fitted();
+  generate_plot_Xbar();
 }
